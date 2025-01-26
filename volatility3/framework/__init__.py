ba6b709aba9496f75a61a806f06719189ac97491
Abyss Watcher <abyss-w4tcher@proton.me>
2025-01-26 10:34:18 +0100

    use VersionMismatchException

--- volatility3/framework/__init__.py
+++ volatility3/framework/__init__.py
@@ -61,25 +61,23 @@ class Deprecation:
     @staticmethod
     def deprecated_method(
         replacement: Callable,
-        replacement_base_class_required_version: Tuple[int, int, int] = None,
+        replacement_version: Tuple[int, int, int] = None,
         additional_information: str = "",
     ):
         """A decorator for marking functions as deprecated.
 
         Args:
             replacement: The replacement function overriding the deprecated API, in the form of a Callable (typically a method)
-            replacement_base_class_required_version: The "replacement" base class version that the deprecated method expects before proxying to it. This implies that "replacement" is a method from a class that inherits from VersionableInterface.
+            replacement_version: The "replacement" base class version that the deprecated method expects before proxying to it. This implies that "replacement" is a method from a class that inherits from VersionableInterface.
             additional_information: Information appended at the end of the deprecation message
         """
 
         def decorator(deprecated_func):
             @functools.wraps(deprecated_func)
             def wrapper(*args, **kwargs):
-                nonlocal replacement, replacement_base_class_required_version, additional_information
+                nonlocal replacement, replacement_version, additional_information
                 # Prevent version mismatches between deprecated (proxy) methods and the ones they proxy
-                if replacement_base_class_required_version is not None and callable(
-                    replacement
-                ):
+                if replacement_version is not None and callable(replacement):
                     # example: replacement = volatility3.MyClass.my_dummy_function
                     # "MyClass.my_dummy_function" -> "MyClass"
                     replacement_base_class_name = replacement.__qualname__.split(".")[0]
@@ -93,26 +91,15 @@ class Deprecation:
                         replacement_base_class,
                         interfaces.configuration.VersionableInterface,
                     ):
-                        # Construct a requirement
-                        req = requirements.VersionRequirement(
-                            name=replacement_base_class.__name__,
-                            component=replacement_base_class,
-                            version=replacement_base_class_required_version,
-                        )
-                        # Verify the requirement
-                        if not req.matches_required(
-                            req._version, req._component.version
+                        # SemVer check
+                        if not requirements.VersionRequirement.matches_required(
+                            replacement_version, replacement_base_class.version
                         ):
-                            full_unsat_req_path = (
-                                deprecated_func.__module__
-                                + "."
-                                + deprecated_func.__qualname__
-                                + "."
-                                + req.name
-                            )
-                            # Catched by the cli and redirected to process_unsatisfied_exceptions
-                            raise exceptions.UnsatisfiedException(
-                                {full_unsat_req_path: req}
+                            raise exceptions.VersionMismatchException(
+                                deprecated_func,
+                                replacement_base_class,
+                                replacement_version,
+                                "A deprecated method was unable to proxy the call to its replacement",
                             )
 
                 deprecation_msg = f"Method \"{deprecated_func.__module__ + '.' + deprecated_func.__qualname__}\" is deprecated, use \"{replacement.__module__ + '.' + replacement.__qualname__}\" instead. {additional_information}"
