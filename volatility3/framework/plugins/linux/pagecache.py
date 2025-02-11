de42e58bfa96cf9208fe8e426716056ff44b2c31
Abyss Watcher <abyss-w4tcher@proton.me>
2025-02-11 17:54:19 +0100

    require framework v2.21.0 for recoverfs

--- volatility3/framework/plugins/linux/pagecache.py
+++ volatility3/framework/plugins/linux/pagecache.py
@@ -638,7 +638,7 @@ class RecoverFs(plugins.PluginInterface):
     """
 
     _version = (1, 0, 0)
-    _required_framework_version = (2, 0, 0)
+    _required_framework_version = (2, 21, 0)
 
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
